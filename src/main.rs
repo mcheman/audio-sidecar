c66441867d1ca3ee5fcf711937639966c5f63f87
Matthew Cheman <mattcheman@pm.me>
2025-01-15 11:50:12 -0500

    Respond to quit event

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 extern crate sdl3_sys;
 
-use std::ffi::{c_float, c_int, c_void, CString};
+use std::ffi::{c_float, c_int, CString};
+use std::mem::zeroed;
 use std::process::exit;
 use std::ptr;
-// use sdl3::pixels::Color;
-// use sdl3::event::Event;
-// use sdl3::keyboard::Keycode;
 use std::time::Duration;
 use sdl3_sys::everything::*;
-// use sdl3::audio::{AudioFormat, AudioSpec};
 
 // todo wrap sdl code in safe crate and hide these variables within, ideally within some created struct
 static mut window: *mut SDL_Window = ptr::null_mut();
@@ -43,6 +40,7 @@ pub fn main() {
         devices = SDL_GetAudioRecordingDevices(&mut num_devices); // free'd below
         if devices.is_null() || num_devices == 0 {
             SDL_Log(c"No recording devices found!".as_ptr(), SDL_GetError());
+            SDL_Quit();
             exit(1);
         }
 
@@ -83,31 +81,37 @@ pub fn main() {
         SDL_BindAudioStream(logical_interface_id, audio_stream);
 
 
-        let mut m = 0;
         loop {
-            let mut samples = [0i32; 368];
+            let mut event: SDL_Event = zeroed();
+            while (SDL_PollEvent(&mut event)) {  // poll until all events are handled!
+                // decide what to do with this event.
+                match SDL_EventType(event.r#type) {
+                    SDL_EventType::QUIT => {
+                        SDL_FlushAudioStream(audio_stream);
+                        SDL_CloseAudioDevice(logical_interface_id);
+                        SDL_Quit();
+                        exit(0);
+                    },
+                    _ => continue
+                }
+            }
+
+            let mut samples = [0i32; 44100/30];
 
             let bytes_read = SDL_GetAudioStreamData(audio_stream, samples.as_mut_ptr().cast(), (samples.len() * 2) as c_int);
 
             let mut max = 0;
             for s in samples {
-                if s != 0 {
-                    println!("{:?}", s);
-                }
                 if s > max {
                     max = s;
                 }
             }
 
-            if max > m {
-                m = max;
-            }
-
             SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
             SDL_RenderClear(renderer);
-            SDL_SetRenderDrawColor(renderer, 255, 150, 255, 255);  /* blue, full alpha */
+            SDL_SetRenderDrawColor(renderer, 255, 150, 255, 255);
 
-            
+
             let rect = SDL_FRect {
                 h: (max as f64 * (480.0 / i32::MAX as f64)) as c_float,
                 w: 100.0,
@@ -118,58 +122,9 @@ pub fn main() {
             SDL_RenderFillRect(renderer, &rect);
 
             SDL_RenderPresent(renderer);
+            ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 120));
         }
     }
 
 
-
-    // let audio_subsystem = sdl_context.audio().unwrap();
-    //
-    // let audio_device_ids = audio_subsystem.audio_recording_device_ids().unwrap();
-    // for (index, audio_device_id) in audio_device_ids.iter().enumerate() {
-    //
-    //     if audio_device_id.name().unwrap().to_lowercase().contains("scarlett") {
-    //         println!("Audio device \"{}\" found", audio_device_id.name().unwrap());
-    //     }
-    // }
-    //
-    // let audio_settings = AudioSpec::new(Some(44100), Some(1), Some(AudioFormat::F32LE));
-    //
-    // audio_subsystem.default_recording_device().
-    
-
-    // let video_subsystem = sdl_context.video().unwrap();
-    // let window = video_subsystem.window("rust-sdl3 demo", 800, 600)
-    //     .position_centered()
-    //     .hidden()
-    //     .build()
-    //     .unwrap();
-    //
-    // let mut canvas = window.into_canvas();
-    // canvas.window_mut().show();
-    //
-    // canvas.set_draw_color(Color::RGB(0, 255, 255));
-    // canvas.clear();
-    // canvas.present();
-    // let mut event_pump = sdl_context.event_pump().unwrap();
-    // let mut i = 0;
-    // 'running: loop {
-    //     i = (i + 1) % 255;
-    //     canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-    //     canvas.clear();
-    //     for event in event_pump.poll_iter() {
-    //         match event {
-    //             Event::Quit {..} |
-    //             Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-    //                 break 'running
-    //             },
-    //             _ => {}
-    //         }
-    //     }
-    //     // The rest of the game loop goes here...
-    //
-    //     canvas.present();
-    //     ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-    // }
-    ::std::thread::sleep(Duration::new(1, 1_000_000_000u32 / 60));
 }
